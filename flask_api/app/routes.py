9bb9ff87170fe22f95f1d2f482da37402c59ba72
yoyoyoseob <yoyoyoseob@gmail.com>
2024-07-03 11:48:14 -0400

    Update json structure to be an array of dicts and make code changes accordingly to support

--- flask_api/app/routes.py
+++ flask_api/app/routes.py
@@ -9,73 +9,82 @@ import json
 #   * Deletion, port over logic to recommend different food to delete
 #   * Email report
 #   * Food class object to wrap name, count, expiry, timestamp
-#   * Commit changes to Ko's repo (https://github.com/SrKoDes/505-api)
 # https://www.moesif.com/blog/technical/api-development/Building-RESTful-API-with-Flask/
 # https://www.w3schools.com/python/python_classes.asp
 
-dateFormat = "%Y%m%d"
-jsonFileName = "expiry.json"
+date_format = "%Y%m%d"
+json_filename = "expiry.json"
 
 def to_date(date_string):
     try:
-        return datetime.strptime(date_string, dateFormat).date()
+        return datetime.strptime(date_string, date_format).date()
     except ValueError:
         raise ValueError('{} is not a valid date in the format YYYYMMDD'.format(date_string))
 
+def get_expiry(element):
+    return element['expiry']
+
 @app.route('/food/', methods=["GET", "POST", "DELETE"])
 def manage_food():
-    with open(jsonFileName, "r") as file:
+    with open(json_filename, "r") as file:
         data = json.load(file)
 
     if request.method == "GET":
-        return sorted(data.items(), key=lambda item: item[1]['expiry'])
+        data.sort(key=get_expiry)
+        return data
 #----------------------------------------------------------------------------------#
-    requestData = request.json
-    food = requestData['food']
-    count = requestData['count']
-    expiry = to_date(requestData['expiry']).strftime(dateFormat)
-    timestamp = datetime.now().date().strftime(dateFormat)
+    request_data = request.json
+    food = request_data['food']
+    count = request_data['count']
+    expiry = to_date(request_data['expiry']).strftime(date_format)
+    timestamp = datetime.now().date().strftime(date_format)
 
     if request.method == "POST":
-        # If dictionary contains "food" with the same expiration date, count += 1
-        # If dictionary contains "food" with different expiration date, "food" = "food - date"
+        # If the array contains "food" with the same expiration date: count += newCount
+        # If the array contains "food" with a different expiration date: "{food} - {expiry}"
 
-        if food in data.keys():
-            savedFood = data[food]
-            if savedFood['expiry'] == expiry:
-                count += savedFood['count']
-            else:
-                food += "-" + expiry
+        for item in data:
+            if item['food'] == food: # Food already exists.
+                if item['expiry'] == expiry: # Food has the same expiry
+                    item['count'] += count # Update the count of the existing item
+
+                    # Do not append a new element, override existing data
+                    with open(json_filename, "w") as file:
+                        json.dump(data, file)
+
+                    return "Updated {} with new count: {}".format(food, item['count'])
+                else: #Implies that the food has a different expiry
+                    food += "-" + expiry
 
         element = {
-            food: {
-                "expiry": expiry,
-                "count": count,
-                "timestamp": timestamp
-            }
+            "food": food,
+            "expiry": expiry,
+            "count": count,
+            "timestamp": timestamp
         }
 
-        data.update(element)
-        with open(jsonFileName, "w") as file:
+        data.append(element)
+        with open(json_filename, "w") as file:
             json.dump(data, file)
 
         return "Added {} of {} that expires {}".format(count, food, expiry)
 #----------------------------------------------------------------------------------#
     elif request.method == "DELETE":
-        if food not in data.keys():
+        if food not in [item['food'] for item in data]:
             return "{} does not exist".format(food)
 
-        savedFood = data.get(food)
-        updatedCount = savedFood['count'] - count
+        for item in data:
+            if item['food'] == food: # Food already exists
+                new_count = item['count'] - count
 
-        if updatedCount < 1:
-            del data[food]
-            message = "Deleted {}".format(food)
-        else:
-            data[food]['count'] = updatedCount
-            message = "{} of {} remaining".format(updatedCount, food)
+                if new_count < 1:
+                    data.remove(item)
+                    message = "Deleted {}".format(food)
+                else:
+                    item['count'] = new_count
+                    message = "{} of {} remaining".format(new_count, food)
 
-        with open(jsonFileName, "w") as file:
+        with open(json_filename, "w") as file:
             json.dump(data, file)
 
         return message
